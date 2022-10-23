1362af4c443b189699ea5b643323022fa9dd57cf
Ivan Kiselev <111705295+ababism@users.noreply.github.com>
2022-10-23 22:20:18 +0300

    Прошедшая тесты программа

---
+++ ACS_HW_1/hw_moded_after_6/task.s
@@ -0,0 +1,63 @@
+	.file	"task.c"
+	.intel_syntax noprefix
+	.text
+	.globl	task
+	.type	task, @function
+task:
+	endbr64
+	push	rbp
+	mov	rbp, rsp
+	mov	QWORD PTR -24[rbp], rdi
+	mov	DWORD PTR -28[rbp], esi
+	mov	QWORD PTR -40[rbp], rdx
+	mov	r13d, 0
+	jmp	.L2
+.L4:
+	mov	eax, r13d
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	edx, DWORD PTR [rax]
+	mov	eax, r13d
+	lea	rcx, 0[0+rax*4]
+	mov	rax, QWORD PTR -40[rbp]
+	add	rax, rcx
+	sub	edx, 5
+	mov	DWORD PTR [rax], edx
+	add	r13d, 1
+.L2:
+	mov	eax, r13d
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	eax, DWORD PTR [rax]
+	test	eax, eax
+	jg	.L5
+	mov	eax, r13d
+	cmp	eax, DWORD PTR -28[rbp]
+	jl	.L4
+	jmp	.L5
+.L6:
+	mov	eax, r13d
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	edx, r13d
+	movsx	rdx, edx
+	lea	rcx, 0[0+rdx*4]
+	mov	rdx, QWORD PTR -40[rbp]
+	add	rdx, rcx
+	mov	eax, DWORD PTR [rax]
+	mov	DWORD PTR [rdx], eax
+	add	r13d, 1
+.L5:
+	mov	eax, r13d
+	cmp	eax, DWORD PTR -28[rbp]
+	jl	.L6
+	nop
+	nop
+	pop	rbp
+	ret
+	.size	task, .-task
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
